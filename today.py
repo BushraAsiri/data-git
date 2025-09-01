b96ada3b770b6f251a26535fb9c15d7e06359d0f
Bushra Asiri <bushraasiri97@gmail.com>
2025-09-01 07:43:10 +0300

    Implement my_name_is and my_buddy_is function

--- today.py
+++ today.py
@@ -1,9 +1,15 @@
-"""A module computing buddy pair names for the day"""
+'''
+A module computing buddy pair names for the day
+'''
 
 def my_name_is():
-   """Return my name as a string"""
+   '''
+   return my name as a string
+   '''
    return "Bushra Asiri"
 
 def my_buddy_is():
-    """Return my buddy's name as a string"""
+    '''
+    return my buddy as a string
+    '''
     return "Layan Al juhani"
